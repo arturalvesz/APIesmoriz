f7fe8c3c2ff6e876cad93768af249975ce7db8ab
arturalvesz <arturalvesz77@gmail.com>
2024-05-04 22:27:53 +0100

    tests

--- routes/jogoRoutes.js
+++ routes/jogoRoutes.js
@@ -9,7 +9,7 @@ router.post("/novo", async (req, res) => {
     // Parse a data no formato "dia-mes-ano" para "ano-mes-dia" (YYYY-MM-DD)
     const [dia, mes, ano] = data.split("-");
     const dataFormatada = `${ano}-${mes}-${dia}`;
-    
+
     const novoJogo = await pool.query(
       "INSERT INTO Jogo (data, hora, nome, escalao_id, resultado) VALUES ($1, $2, $3, $4, $5) RETURNING *",
       [dataFormatada, hora, nome, escalao_id, resultado]
@@ -25,6 +25,17 @@ router.post("/novo", async (req, res) => {
 router.get("/all", async (req, res) => {
   try {
     const todosJogos = await pool.query("SELECT * FROM Jogo");
+    res.json(todosJogos.rows);
+  } catch (err) {
+    console.error("Erro ao obter jogos:", err);
+    res.status(500).json({ error: "Erro ao obter jogos" });
+  }
+});
+
+router.get("/all/:escalao_id", async (req, res) => {
+  try {
+    const { escalao_id } = req.params;
+    const todosJogos = await pool.query("SELECT * FROM Jogo WHERE escalao_id = $1", [escalao_id]);
     res.json(todosJogos.rows);
   } catch (err) {
     console.error("Erro ao obter jogos:", err);
