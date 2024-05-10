dc2b28e5fd54fd7363e798f6ec8cb3b7f7af6057
arturalvesz <arturalvesz77@gmail.com>
2024-05-10 20:28:31 +0100

    stripe testing

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -1,48 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../dbConfig");
+const stripe = require('stripe')('STRIPE_SECRET_KEY');
 
-// Rota para criar um pagamento
-router.post("/pagamento", async (req, res) => {
-  try {
-    // Recupera os dados do bilheteira enviados pelo cliente
-    const { quantidade ,nbilhetes, jogo_id, preco_normal, preco_socio, token } = req.body;
-
-    // Verifica se os dados do bilheteira foram recebidos
-    if (!nbilhetes || !jogo_id || !token) {
-      return res.status(400).json({ error: "Dados do bilheteira incompletos" });
-    }
-
-    // Calcula o montante total do pagamento com base no número de bilhetes e nos preços
-    const amount = quantidade * preco_normal;
-
-    // Cria uma cobrança usando o token
-    const charge = await stripe.charges.create({
-      amount: amount * 100, // Montante em centavos
-      currency: "eur", // Moeda (pode ser 'usd', 'eur', etc.)
-      description: `Compra de bilhetes para o jogo ID ${jogo_id}`,
-      source: token, // Token de pagamento gerado pelo Stripe.js
-    });
-
-    // Se a cobrança for bem-sucedida, insere os dados do bilheteira no banco de dados
-    if (charge) {
-      const queryText =
-        "INSERT INTO bilhete (bilheteira_id) VALUES ($1)";
-      await pool.query(queryText, [
-        bilheteira_id
-      ]);
-    }
-
-    // Envie uma resposta de sucesso
-    res
-      .status(200)
-      .json({ message: "Pagamento processado com sucesso", charge });
-  } catch (error) {
-    // Se ocorrer um erro, envia uma resposta de erro
-    console.error("Erro ao processar o pagamento:", error);
-    res.status(500).json({ error: "Erro ao processar o pagamento" });
-  }
-});
-
+router.post("/create-checkout-session", async (req, res) => { 
+    const { nome, precoNormal, quantidade } = req.body; 
+    const session = await stripe.checkout.sessions.create({ 
+      payment_method_types: ["card"], 
+      line_items: [ 
+        { 
+          price_data: { 
+            currency: "eur", 
+            product_data: { 
+              name: nome, 
+            }, 
+            unit_amount: precoNormal * 100, 
+          }, 
+          quantity: quantidade, 
+        }, 
+      ], 
+      mode: "payment",
+    }); 
+    res.redirect(303, session.url);
+  }); 
 
 module.exports = router;
