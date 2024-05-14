bb2add3b230cf25d7918604b1af4c3081d877522
arturalvesz <arturalvesz77@gmail.com>
2024-05-14 14:34:48 +0100

    fadfad

--- routes/stripeWebhookRoutes.js
+++ routes/stripeWebhookRoutes.js
@@ -45,6 +45,13 @@ async function criarBilhete(bilheteiraId, dataValidade, quantidade, dataCompra, 
   try {
     const bilheteiraIdInt = parseInt(bilheteiraId);
     const utilizadorIdInt = parseInt(utilizadorId);
+
+    console.log("Valores recebidos para criar bilhete: \n");
+    console.log("Bilheteira ID: \n", bilheteiraIdInt);
+    console.log("Data de validade: \n", dataValidade);
+    console.log("Quantidade: \n", quantidade);
+    console.log("Data de compra: \n", dataCompra);
+    console.log("Utilizador ID: \n", utilizadorIdInt);
     
     const query = "INSERT INTO bilhete (bilheteira_id, data_validade, data_compra, utilizador_id) VALUES ($1, $2, $3, $4)";
     const values = [bilheteiraIdInt, dataValidade, dataCompra, utilizadorIdInt];
