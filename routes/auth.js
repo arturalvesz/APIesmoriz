f791da0518b8bf0191d30cd000213cff4f651e37
arturalvesz <arturalvesz77@gmail.com>
2024-05-16 13:28:36 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -60,21 +60,24 @@ router.get('/get-user-id/:token', async (req, res) => {
         res.status(400).json({ error: 'Token inválido ou expirado' });
     }   
 });
-
 // Dados do usuário autenticado
-router.get('/user-info', authenticateToken, async (req, res) => {
+router.get('/user-info/:token', async (req, res) => {
+    const token = req.params.token;
+    if (!token) return res.status(401).send('Token not provided');
+    
     try {
-        // O ID do usuário está contido no objeto req.user definido pelo middleware authenticateToken
-        const userId = req.user.id;
+        const decodedToken = jwt.verify(token, JWT_SECRET);
+        const userId = decodedToken.id;
         const result = await pool.query('SELECT nome, email FROM utilizador WHERE id = $1', [userId]);
         const user = result.rows[0];
         if (!user) return res.status(404).send('User not found');
         res.json({ authUser: { nome: user.nome, email: user.email } });
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error fetching user data');
+        res.status(400).json({ error: 'Invalid or expired token' });
     }
 });
+
 
 // Rota para login de usuário
 router.post('/login', async (req, res) => {
