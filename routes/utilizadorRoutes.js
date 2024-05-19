222738318fb4500423155432c8fa9d1fca43dec8
arturalvesz <arturalvesz77@gmail.com>
2024-05-19 16:58:14 +0100

    fix

--- routes/utilizadorRoutes.js
+++ routes/utilizadorRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../dbConfig');
+const admin = require("firebase-admin");
 
 const serviceAccount = require('../serviceAccountKey.json');
 
