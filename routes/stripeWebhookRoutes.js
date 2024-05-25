bab858d769b4cd94be1ca0fae5321f25b18bceec
arturalvesz <arturalvesz77@gmail.com>
2024-05-25 12:25:25 +0100

    update

--- routes/stripeWebhookRoutes.js
+++ routes/stripeWebhookRoutes.js
@@ -54,9 +54,13 @@ router.post("/webhook", async (req, res) => {
         res.status(200).send();
     }
     }else if (event.type === 'customer.subscription.updated') {
+      const subscriptionId = event.data.object.subscription;
+      const subscription = await stripe.subscriptions.retrieve(subscriptionId);
       await handleSubscriptionUpdate(subscription);
       res.status(200).send();
     }else if(event.type === 'customer.subscription.deleted') {
+      const subscriptionId = event.data.object.subscription;
+      const subscription = await stripe.subscriptions.retrieve(subscriptionId);
       await handleSubscriptionUpdate(subscription);
       res.status(200).send();
     }
