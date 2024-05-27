a431d85a3baa20176852ae8a0057d705dbe27688
arturalvesz <arturalvesz77@gmail.com>
2024-05-27 20:59:51 +0100

    update

--- routes/fotoRoutes.js
+++ routes/fotoRoutes.js
@@ -2,14 +2,39 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../dbConfig');
 
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dsy8o6tn7', 
+  api_key: '519515781627635', 
+  api_secret: 'Wlbjy2udigy1NfjLwX__SRPY2pc' 
+});
+
 // Criar uma nova foto
 router.post('/novo', async (req, res) => {
   try {
-    const { path, evento_id,noticia_id, patrocinador_id } = req.body;
+    // Check if a file is uploaded
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({ error: 'Nenhuma foto enviada' });
+    }
+
+    // Get the uploaded file from the request
+    const { file } = req.files;
+
+    // Upload the file to Cloudinary
+    const uploadedPhoto = await cloudinary.uploader.upload(file.tempFilePath);
+
+    // Extract the photo URL from the Cloudinary response
+    const { secure_url: photoUrl, public_id: cloudinaryId } = uploadedPhoto;
+
+    // Store the photo URL and associated data in the database
+    const { evento_id, noticia_id, patrocinador_id } = req.body;
     const novaFoto = await pool.query(
-      'INSERT INTO foto (path, atleta_id, patrocinador_id) VALUES ($1, $2, $3) RETURNING *',
-      [path, noticia_id || null,evento_id || null, patrocinador_id || null]
+      'INSERT INTO foto (path, evento_id, noticia_id, patrocinador_id) VALUES ($1, $2, $3, $4) RETURNING *',
+      [photoUrl, evento_id || null, noticia_id || null, patrocinador_id || null]
     );
+
+    // Return the newly created photo
     res.json(novaFoto.rows[0]);
   } catch (err) {
     console.error('Erro ao criar foto:', err);
@@ -63,10 +88,10 @@ router.get('/patrocinador/:id', async (req, res) => {
 router.put('/update/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const { path, atleta_id, patrocinador_id } = req.body;
+    const { path,patrocinador_id } = req.body;
     const fotoAtualizada = await pool.query(
-      'UPDATE foto SET path = $1, atleta_id = $2, patrocinador_id = $3 WHERE id = $4 RETURNING *',
-      [path, atleta_id || null, patrocinador_id || null, id]
+      'UPDATE foto SET path = $1, patrocinador_id = $2 WHERE id = $3 RETURNING *',
+      [path, patrocinador_id || null, id]
     );
     if (fotoAtualizada.rows.length === 0) {
       return res.status(404).json({ error: 'Foto não encontrada' });
