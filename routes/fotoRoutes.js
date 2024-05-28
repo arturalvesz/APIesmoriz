ed0abfcf1c2981593bb6041d2706c2d5f9b8c3c6
arturalvesz <arturalvesz77@gmail.com>
2024-05-28 08:49:40 +0100

    test

--- routes/fotoRoutes.js
+++ routes/fotoRoutes.js
@@ -15,7 +15,7 @@ cloudinary.config({
 var storage = multer.diskStorage({
 
   destination: function(req, file, cb){
-    cb(null, '../public/uploads')
+    cb(null, './public/uploads')
   },
   filename: function(req, file, cb){
     cb(null, file.originalname)
